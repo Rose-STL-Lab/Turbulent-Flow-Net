b832365c47980987f789e9a756fef263f0f9694a
Ray <32885937+Rui1521@users.noreply.github.com>
2019-12-21 19:57:34 -0500

    Update SST.py

--- Baselines/SST/SST.py
+++ Baselines/SST/SST.py
@@ -21,6 +21,40 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 import warnings
 warnings.filterwarnings("ignore")
 
+class DenseGridGen(nn.Module):
+    def __init__(self, transpose=True):
+        super(DenseGridGen, self).__init__()
+        self.transpose = transpose
+        self.register_buffer('grid', torch.Tensor())
+
+    def forward(self, x):
+
+        if self.transpose:
+            x = x.transpose(1, 2).transpose(2, 3)
+        
+        g0 = torch.linspace(-1, 1, x.size(2)
+                            ).unsqueeze(0).repeat(x.size(1), 1)
+        
+        g1 = torch.linspace(-1, 1, x.size(1)
+                            ).unsqueeze(1).repeat(1, x.size(2))
+        grid = torch.cat([g0.unsqueeze(-1), g1.unsqueeze(-1)], -1)
+        self.grid.resize_(grid.size()).copy_(grid)
+
+        bgrid = Variable(self.grid)
+        bgrid = bgrid.unsqueeze(0).expand(x.size(0), *bgrid.size())
+
+        return bgrid - x
+    
+class GaussianWarpingScheme(nn.Module):
+    def __init__(self, padding_mode='zeros', std=0.25):
+        super(GaussianWarpingScheme, self).__init__()
+        self.grid = DenseGridGen()
+        self.std = std
+        self.padding_mode = padding_mode
+
+    def forward(self, im, w):
+        return F.grid_sample(im, self.grid(w), padding_mode=self.padding_mode, mode='bilinear')
+      
 train_direc = "/global/cscratch1/sd/rwang2/Data/data_64/train_seqs/train_sample_"
 test_direc = "/global/cscratch1/sd/rwang2/Data/data_64/test_seqs/test_sample_"
 input_length = 24
