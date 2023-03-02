4cfb43de714aece790b0688e2ca638c35e602bba
lakshya <llnu@ucsd.edu>
2023-03-01 18:07:49 -0800

    aimstack

---
+++ TF_net/args.py
@@ -0,0 +1,122 @@
+import argparse
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--min_mse",
+                        type=int,
+                        default=1)
+    parser.add_argument("--seed",
+                        type=int,
+                        default=53)
+    parser.add_argument("--time_range",
+                        type=int,
+                        default=6)
+    parser.add_argument("--output_length",
+                        type=int,
+                        default=4)
+    parser.add_argument("--input_length",
+                        type=int,
+                        default=27)
+    parser.add_argument("--learning_rate",
+                        type=float,
+                        default=0.001)
+    parser.add_argument("--dropout_rate",
+                        type=int,
+                        default=0)
+    parser.add_argument("--kernel_size",
+                        type=int,
+                        default=3)
+    parser.add_argument("--batch_size",
+                        type=int,
+                        default=32)
+    parser.add_argument("--coef",
+                        type=float,
+                        default=0)
+    parser.add_argument("--coef2",
+                        type=float,
+                        default=1)
+    parser.add_argument("--step_size",
+                        type=int,
+                        default=1)
+    parser.add_argument("--path",
+                        type=str,
+                        default="./results/")
+    parser.add_argument("--epoch",
+                        type=int,
+                        default=100)
+    parser.add_argument("--mide",
+                        type=float,
+                        default=None)
+    parser.add_argument("--m_init",
+                        type=float,
+                        default=0.09)
+    parser.add_argument("--slope",
+                        type=int,
+                        default=300) # 300
+    parser.add_argument("--slope_init",
+                        help="if slope is None, i.e. slope is learnt, init it with this value",
+                        type=int,
+                        default=300) # 300  # Reduce this value if you get a nan gradient
+    parser.add_argument("--barrier",
+                        type=float,
+                        default=1e-3)
+    parser.add_argument("--d_ids",
+                        nargs='+',
+                        type=int,
+                        default=[2])
+    parser.add_argument("--bnorm", 
+                        action='store_true', 
+                        default=False)
+    parser.add_argument("--no_weight", 
+                        action='store_true', 
+                        default=False)
+    parser.add_argument("--only_val", 
+                        action='store_true', 
+                        default=False)
+    parser.add_argument("--use_time", 
+                        help="use time as well for (mse - mide) transformation", 
+                        action='store_true', default=False)
+    parser.add_argument("--time_factor",
+                        type=float,
+                        default=0.01)
+    parser.add_argument("--desc",
+                        type=str,
+                        default="")
+    parser.add_argument("--data",
+                        type=str,
+                        default="rbc_data.pt")
+    parser.add_argument("--outln_steps",
+                        type=int,
+                        default=0)
+    parser.add_argument("--outln_init",
+                        type=int,
+                        help="init if warmuping",
+                        default = 4) 
+    parser.add_argument("--outln_stride",
+                        type=int,
+                        help="stride to take, if None, then stride = args.output_length - args.outln_init, that is one jump",
+                        default = None)  
+    parser.add_argument("--noise",
+                        type=int,
+                        help="sigma = 0.01*noise",
+                        default = 0.0)
+    parser.add_argument("--dnsn",
+                        help="'do not scale noise', multiply gaussian with the element",
+                        default = False,
+                        action="store_true")
+    parser.add_argument("--wt_decay",
+                            help="weight decay",
+                            default=4e-4,
+                            type=float) 
+    parser.add_argument("--addon_enc",
+                            help="number of additonal layers to encoder",
+                            type=int,
+                            default = 0)
+    parser.add_argument("--addon_dec",
+                            help="number of additonal layers to decoder",
+                            type=int,
+                            default = 0)
+    parser.add_argument("--not_use_test_mode",
+                        action="store_true",
+                        default=False,
+                        help="test with same config as train mode")
+    return parser.parse_args()
--- TF_net/run_model.py
+++ TF_net/run_model.py
@@ -20,138 +20,21 @@ import warnings
 warnings.filterwarnings("ignore")
 import argparse
 from icecream import ic as ic_print
+from args import parse_arguments
+import aim
 
 # Reproducibility
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
 torch.use_deterministic_algorithms(True)
-
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--min_mse",
-                        type=int,
-                        default=1)
-    parser.add_argument("--seed",
-                        type=int,
-                        default=53)
-    parser.add_argument("--time_range",
-                        type=int,
-                        default=6)
-    parser.add_argument("--output_length",
-                        type=int,
-                        default=4)
-    parser.add_argument("--input_length",
-                        type=int,
-                        default=26)
-    parser.add_argument("--learning_rate",
-                        type=float,
-                        default=0.001)
-    parser.add_argument("--dropout_rate",
-                        type=int,
-                        default=0)
-    parser.add_argument("--kernel_size",
-                        type=int,
-                        default=3)
-    parser.add_argument("--batch_size",
-                        type=int,
-                        default=32)
-    parser.add_argument("--coef",
-                        type=float,
-                        default=0)
-    parser.add_argument("--coef2",
-                        type=float,
-                        default=1)
-    parser.add_argument("--step_size",
-                        type=int,
-                        default=1)
-    parser.add_argument("--path",
-                        type=str,
-                        default="./results/")
-    parser.add_argument("--epoch",
-                        type=int,
-                        default=100)
-    parser.add_argument("--mide",
-                        type=float,
-                        default=None)
-    parser.add_argument("--m_init",
-                        type=float,
-                        default=0.09)
-    parser.add_argument("--slope",
-                        type=int,
-                        default=300) # 300
-    parser.add_argument("--slope_init",
-                        help="if slope is None, i.e. slope is learnt, init it with this value",
-                        type=int,
-                        default=300) # 300  # Reduce this value if you get a nan gradient
-    parser.add_argument("--barrier",
-                        type=float,
-                        default=1e-3)
-    parser.add_argument("--d_ids",
-                        nargs='+',
-                        type=int,
-                        default=[2])
-    parser.add_argument("--bnorm", 
-                        action='store_true', 
-                        default=False)
-    parser.add_argument("--no_weight", 
-                        action='store_true', 
-                        default=False)
-    parser.add_argument("--only_val", 
-                        action='store_true', 
-                        default=False)
-    parser.add_argument("--use_time", 
-                        help="use time as well for (mse - mide) transformation", 
-                        action='store_true', default=False)
-    parser.add_argument("--time_factor",
-                        type=float,
-                        default=0.01)
-    parser.add_argument("--desc",
-                        type=str,
-                        default="")
-    parser.add_argument("--data",
-                        type=str,
-                        default="rbc_data.pt")
-    parser.add_argument("--outln_steps",
-                        type=int,
-                        default=0)
-    parser.add_argument("--outln_init",
-                        type=int,
-                        help="init if warmuping",
-                        default = 4) 
-    parser.add_argument("--outln_stride",
-                        type=int,
-                        help="stride to take, if None, then stride = args.output_length - args.outln_init, that is one jump",
-                        default = None)  
-    parser.add_argument("--noise",
-                        type=int,
-                        help="sigma = 0.01*noise",
-                        default = 0.0)
-    parser.add_argument("--dnsn",
-                        help="'do not scale noise', multiply gaussian with the element",
-                        default = False,
-                        action="store_true")
-    parser.add_argument("--wt_decay",
-                            help="weight decay",
-                            default=4e-4,
-                            type=float) 
-    parser.add_argument("--addon_enc",
-                            help="number of additonal layers to encoder",
-                            type=int,
-                            default = 0)
-    parser.add_argument("--addon_dec",
-                            help="number of additonal layers to decoder",
-                            type=int,
-                            default = 0)
-    parser.add_argument("--not_use_test_mode",
-                        action="store_true",
-                        default=False,
-                        help="test with same config as train mode")
-    return parser.parse_args()
                         
 
 
 args = parse_arguments()
 args.use_test_mode = not args.not_use_test_mode
+
+run = aim.Run(experiment=args.data)
+run['args'] = vars(args)
 
 torch.manual_seed(args.seed)
 random.seed(args.seed)
@@ -182,6 +65,7 @@ elif 'data' in args.data and '101' in args.data:
 else:
     raise ValueError("Un expected data file name")
 args.transform = Scaler(transform_type, offset)
+run['transform'] = vars(args.transform)
 
 data_prep = preprocess(args, permute, compress, test_mode_train)
 device_ids = args.d_ids
@@ -227,6 +111,7 @@ regularizer = DivergenceLoss(torch.nn.MSELoss()) #Has cuda leak to zeroth device
 coef = args.coef
 coef2 = args.coef2
 print("configs:",[min_mse,time_range,output_length,input_length,learning_rate,dropout_rate,kernel_size,batch_size,coef,coef2,step_size,args])
+
 
 if args.mide is None or args.slope is None:
     assert args.mide is None
@@ -282,16 +167,20 @@ for i in range(args.epoch):
     mse, val_reg_rst,preds, trues = eval_epoch(valid_loader, model, loss_fun,coef2,barrier=args.barrier,mide=args.mide,slope=args.slope, device=device)
     valid_mse.append(mse)
     val_reg.append(val_reg_rst)
+    run.track({'train_mse': train_mse, 'train_reg': train_reg_rst}, context={'subset': 'train'}, epoch=i)
+    run.track({'val_mse': mse, 'val_reg': val_reg_rst}, context={'subset': 'val'}, epoch=i)
     if valid_mse[-1] < min_mse:
         min_mse = valid_mse[-1]   
         torch.save(model, args.path+"model.pth")
         torch.save(model.module.state_dict(), args.path+"module_stdict.pth")
+        run.track(min_mse, name='min_valid_mse', epoch=i)
     end = time.time()
     # change 50 to 100
     #if (len(train_mse) > 50 and np.mean(valid_mse[-5:]) >= np.mean(valid_mse[-10:-5])):
     #        break
     ic_print(i, train_mse[-1],train_reg[-1], valid_mse[-1],val_reg[-1], round((end-start)/60,5))
     if m_pred is not None:
+        run.track({'m_pred_weight': m_pred.m_pred.weight, 'm_pred.bias': m_pred.m_pred.bias, 'm_pred.slope': m_pred.slope}, epoch=i)
         ic_print(m_pred.m_pred.weight, m_pred.m_pred.bias, m_pred.slope)
 ic_print(time_range, min_mse)
 
@@ -322,6 +211,8 @@ preds, trues, loss_curve = test_epoch(args, test_loader, best_model, loss_fun,te
 
 torch.save({"loss_curve": loss_curve}, 
             args.path+f"results_val{'' if args.use_test_mode else '_64'}.pt",pickle_protocol=5)
+for i, lc in enumerate(loss_curve):
+    run.track(lc, name=f"val_test_curve{'' if args.use_test_mode else '_64'}", step=i)
 
 # On test set
 if not args.only_val:
@@ -332,3 +223,5 @@ if not args.only_val:
 
     torch.save({"loss_curve": loss_curve}, 
                 args.path+f"results{'' if args.use_test_mode else '_64'}.pt",pickle_protocol=5)
+    for i, lc in enumerate(loss_curve):
+        run.track(lc, name=f"test_curve{'' if args.use_test_mode else '_64'}", step=i)
